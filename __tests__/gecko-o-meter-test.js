3f20bdc0a38802d3011a974b4aab0dac8b5b773b
Jason Liggi <jason@liggi.net>
2016-05-07 12:01:59 +0100

    Adding markup for GeckoOMeter + tests

--- __tests__/gecko-o-meter-test.js
+++ __tests__/gecko-o-meter-test.js
@@ -5,6 +5,10 @@ import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import GeckoOMeter from '../src/app/components/GeckoOMeter';
 
+function has_child_with_class(children, className) {
+  return children.some(c => c.props.className == className);
+}
+
 describe('Gecko-O-Meter',() => {
   let component = (<GeckoOMeter />);
   let result;
@@ -17,6 +21,22 @@ describe('Gecko-O-Meter',() => {
 
   it('should exist', () => {
     expect(result.props.className).toEqual('gecko-o-meter');
+  });
+
+  it('have markup for a needle', () => {
+    expect(has_child_with_class(result.props.children, "gecko-o-meter__needle")).toBeTruthy();
+  });
+
+  it('have markup for a current value', () => {
+    expect(has_child_with_class(result.props.children, "gecko-o-meter__current-value")).toBeTruthy();
+  });
+
+  it('have markup for a min value', () => {
+    expect(has_child_with_class(result.props.children, "gecko-o-meter__min-value")).toBeTruthy();
+  });
+
+  it('have markup for a max value', () => {
+    expect(has_child_with_class(result.props.children, "gecko-o-meter__max-value")).toBeTruthy();
   });
 
 });
--- src/app/components/GeckoOMeter.js
+++ src/app/components/GeckoOMeter.js
@@ -3,7 +3,12 @@ import React from 'react';
 class GeckoOMeter extends React.Component {
   render() {
     return (
-      <div className="gecko-o-meter"></div>
+      <div className="gecko-o-meter">
+        <span className="gecko-o-meter__needle"></span>
+        <span className="gecko-o-meter__current-value"></span>
+        <span className="gecko-o-meter__min-value"></span>
+        <span className="gecko-o-meter__max-value"></span>
+      </div>
     );
   }
 
